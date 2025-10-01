49c22d162679b235651e05f608b0a3137033e1e0
Aiomedin Chorobaev <eliothab58@gmail.com>
2025-10-02 01:34:45 +0600

    fix

--- src/telegram/telegram.controller.ts
+++ src/telegram/telegram.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, HttpCode, Inject, Post } from '@nestjs/common';
+import { Controller, HttpCode, Inject, Post, Req, Res } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { FastifyRequest, FastifyReply } from 'fastify';
 import { Bot, webhookCallback } from 'grammy';
 
 @Controller('telegram')
 export class TelegramController {
-    constructor(@Inject('TELEGRAM_BOT') private readonly bot: Bot) { }
+    constructor(
+        @Inject('TELEGRAM_BOT') private readonly bot: Bot,
+        private readonly config: ConfigService
+    ) { }
 
     @Post('webhook')
     @HttpCode(200)
-    async webhook() {
-        return webhookCallback(this.bot, 'fastify')
+    async webhook(@Req() req: FastifyRequest, @Res() res: FastifyReply) {
+        const secret = this.config.get<string>('TG_WEBHOOK_SECRET');
+        return webhookCallback(this.bot, 'fastify', { secretToken: secret })(req, res);
     }
 }
